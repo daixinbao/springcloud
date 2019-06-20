a83ca2d06d1c7adcb72a98264be0940e050d10cb
daixinbao <daixinbao@braodtext.com.cn>
2019-06-20 12:40:08 +0800

    satge2_commit3

--- eurekaclient/src/main/java/com/scc/stage1/controller/HelloController.java
+++ eurekaclient/src/main/java/com/scc/stage1/controller/HelloController.java
@@ -1,10 +1,15 @@
 package com.scc.stage1.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/***
+ * RefreshScope 自动刷新配置
+ */
 @RestController
+@RefreshScope
 public class HelloController {
 
     @Value("${server.port}")
